d409c8dd78499b5162740fd73aca8a93de9c27e4
vanitanaidu <vanita112003@gmail.com>
2018-08-30 16:47:28 -0400

    made changes to Leaderboard container

--- src/containers/Leaderboard.js
+++ src/containers/Leaderboard.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import _ from 'lodash';
 
 class Leaderboard extends Component {
   constructor(props) {
@@ -10,16 +11,62 @@ class Leaderboard extends Component {
   }
 
   componentDidMount = () => {
-    fetch('http://localhost:3001/api/leaderboards')
+    fetch('http://localhost:3000/api/leaderboards')
       .then(response => response.json())
-      .then(data => console.log(data));
-
+      .then(data => this.setState({ data }))
+      debugger
     }
 
 
 
+    renderDetails = () => {
+      const { data } = this.state
+      if(!data) {
+            return "loading..."
+          } else {
+            _.map(data, function(value, key) {
+debugger
+              while (key < 5) {
+            return (
+
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            )}
+        }
+      )}
+  }
+
+
   render() {
-      return <div>LeaderBoard</div>
+
+    return (
+
+      <div className="container">
+      <div className="row">
+        <div className="col-sm-12 home-checkin-div">
+          <h1 className="home-heading">Meet Your Friendly Competitors</h1>
+          </div>
+        </div>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Member</th>
+                <th>Primary WeWork</th>
+                <th># of Check-Ins</th>
+              </tr>
+            </thead>
+            <tbody>
+          {this.renderDetails()}
+            </tbody>
+          </table>
+          </div>
+
+    )
   }
 }
 
